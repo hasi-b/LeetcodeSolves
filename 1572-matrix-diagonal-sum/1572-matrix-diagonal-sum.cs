72dd0fe0bf105c2860f7cba6ce405a65dbfa4c3e
Hasi-b <hasib.hasan9@gmail.com>
2022-11-15 13:49:38 +0600

    Time: 97 ms (92.36%), Space: 41.6 MB (17.36%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cs
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cs
@@ -10,18 +10,8 @@ public class Solution {
         for(int i=0;i<n;i++){
             
             
-            for(int j=0;j<n;j++){
-                
-                if(i==j){
-                    
-                    sum=sum+mat[i][j];
-                }
-                if(j==n-i-1){
-                    
-                    sum=sum+mat[i][j];
-                }
-                
-            }
+            sum=sum+mat[i][i] + mat[i][n-i-1];
+            
         }
         
         
